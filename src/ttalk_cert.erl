0fc4ad0ed79d6e9ebbd013e5708156e51c1b74aa
David.Gao <david.alpha.fox@gmail.com>
2016-05-09 19:51:27 +0800

    Certificate

--- src/ttalk_cert.erl
+++ src/ttalk_cert.erl
@@ -1,8 +1,17 @@
 -module(ttalk_cert).
 -author("David.Alpha.Fox@gmail.com").
--export([cert_public_key/1]).
-cert_public_key(X509) ->
-	#'OTPCertificate'{tbsCertificate = TBS} = public_key:pkix_decode_cert(X509, otp),
+
+-include_lib("public_key/include/public_key.hrl").
+
+-export([public_key/1]).
+
+public_key(RawX509) ->
+	OTPX509 = decode_otp(RawX509),
+	#'OTPCertificate'{tbsCertificate = TBS} = OTPX509,
 	PublicKey = TBS#'OTPTBSCertificate'.subjectPublicKeyInfo,
 	#'RSAPublicKey'{modulus = N,publicExponent = E} = PublicKey#'OTPSubjectPublicKeyInfo'.subjectPublicKey,
 	[E,N].
+
+decode_otp(Bin)->
+	[{'Certificate', Data, not_encrypted}] = public_key:pem_decode(Bin),
+	public_key:pkix_decode_cert(Data, otp).
