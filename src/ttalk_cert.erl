e6642ee66e1ae3a74cd7f364e17476dcd14ffe0f
David.Gao <david.alpha.fox@gmail.com>
2016-05-09 20:55:00 +0800

    retrive public key

--- src/ttalk_cert.erl
+++ src/ttalk_cert.erl
@@ -3,14 +3,40 @@
 
 -include_lib("public_key/include/public_key.hrl").
 
--export([rsa_public_key/1]).
+-export([public_key/1]).
 
-rsa_public_key(RawX509) ->
+public_key(RawX509) ->
 	OTPX509 = decode_otp(RawX509),
 	#'OTPCertificate'{tbsCertificate = TBS} = OTPX509,
-	PublicKey = TBS#'OTPTBSCertificate'.subjectPublicKeyInfo,
-	#'RSAPublicKey'{modulus = N,publicExponent = E} = PublicKey#'OTPSubjectPublicKeyInfo'.subjectPublicKey,
-	[E,N].
+    Signature = TBS#'OTPTBSCertificate'.signature,
+    Algorithm = Signature#'SignatureAlgorithm'.algorithm,
+    public_key(Algorithm,TBS).
+
+public_key('sha1WithRSAEncryption',TBS) ->
+    PublicKey = TBS#'OTPTBSCertificate'.subjectPublicKeyInfo,
+    #'RSAPublicKey'{modulus = N,publicExponent = E} = PublicKey#'OTPSubjectPublicKeyInfo'.subjectPublicKey,
+    {{rsa,sha},[E,N]};
+
+public_key('sha224WithRSAEncryption',TBS) ->
+    PublicKey = TBS#'OTPTBSCertificate'.subjectPublicKeyInfo,
+    #'RSAPublicKey'{modulus = N,publicExponent = E} = PublicKey#'OTPSubjectPublicKeyInfo'.subjectPublicKey,
+    {{rsa,sha224},[E,N]};
+
+public_key('sha256WithRSAEncryption',TBS)->
+    PublicKey = TBS#'OTPTBSCertificate'.subjectPublicKeyInfo,
+    #'RSAPublicKey'{modulus = N,publicExponent = E} = PublicKey#'OTPSubjectPublicKeyInfo'.subjectPublicKey,
+    {{rsa,sha256},[E,N]};
+
+public_key('sha384WithRSAEncryption',TBS) ->
+    PublicKey = TBS#'OTPTBSCertificate'.subjectPublicKeyInfo,
+    #'RSAPublicKey'{modulus = N,publicExponent = E} = PublicKey#'OTPSubjectPublicKeyInfo'.subjectPublicKey,
+    {{rsa,sha384},[E,N]};
+
+public_key('sha512WithRSAEncryption',TBS) ->
+    PublicKey = TBS#'OTPTBSCertificate'.subjectPublicKeyInfo,
+    #'RSAPublicKey'{modulus = N,publicExponent = E} = PublicKey#'OTPSubjectPublicKeyInfo'.subjectPublicKey,
+    {{rsa,sha512},[E,N]}.
+
 
 decode_otp(Bin)->
 	[{'Certificate', Data, not_encrypted}] = public_key:pem_decode(Bin),
